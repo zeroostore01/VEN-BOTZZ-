ab22f79e41e450e977adbc17059581a6e182f540
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-27 09:10:53 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6283890585717'] //ur owner number
+global.ownername = "𝐓𝐚𝐬𝐲𝐚𝐚" //ur owner name
+global.ytname = "G puny" //ur yt chanel name
+global.socialm = "https://chat.whatsapp.com/E8IwFUibPjkDgilRKw6JBg" //ur github or insta name
+global.location = "Jepang, Tokyo," //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['6283890585717']
+global.ownertag = '62' //ur tag number
+global.botname = '𝒁 𝑬 𝑹 𝑶 𝑴𝑫' //ur bot name
+global.linkz = "https://chat.whatsapp.com/E8IwFUibPjkDgilRKw6JBg" //your theme url which will be displayed on whatsapp
+global.websitex = "https://tinyurl.com/2el4odke" //ur website to be displayed
+global.botscript = 'Past Elu Tahu Banh' //script link
+global.reactmoji = "🗿" //ur menu react emoji
+global.themeemoji = "🥴" //ur theme emoji
+global.packname = "𝒁 𝑬 𝑹 𝑶 Stickers\n\n\n\n\n\n\n\n\n\n\n𝐓𝐚𝐬𝐲𝐚𝐚 " //ur sticker watermark packname
+global.author = "𝒁 𝑬 𝑹 𝑶 𝑴𝑫" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6283890585717'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -106,13 +106,13 @@ global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
+    admin: 'Fitur Hanya Admin Doang',
+    botAdmin: 'Bot Bukan Admin!',
+    owner: 'Fitur Ini digunakan Dengan Developer Bot!',
+    group: 'Fitur Ini Hanya Di Group Doang',
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
+    wait: 'Sabarr.',
     linkm: 'Where is the link?',
     error: 'Error!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
